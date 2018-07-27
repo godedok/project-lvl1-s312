425523970aa5f4ce9b22b9eb275feb459b19a377
godedok <gdededok@gmail.com>
2018-07-27 10:56:06 +0300

    delete comments

--- src/Cli.php
+++ src/Cli.php
@@ -1,29 +1,10 @@
 <?php
-/**
- * Namespace
- *
- * PHP version 7
- *
- * @category PHP
- *
- * @package PHP_Braingames
- *
- * @author Name <username@example.com>
- *
- * @license Name https://github.com/godedok
- *
- * @link https://github.com/
- */
+
 namespace BrainGames\Cli;
 
 use function \cli\line;
 use function \cli\prompt;
 
-/**
- * Function
- *
- * @return string
- */
 function run()
 {
     line('Welcome to the Brain Game!');
